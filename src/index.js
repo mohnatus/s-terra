d6c05f7922975535432ed3177fd1f8d0362e7b0d
FurryCat <hyp-hyper.cat@yandex.ru>
2021-10-22 10:24:15 +0300

    slider

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
+import Swiper, { Navigation, Pagination } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 import './style.scss';
+
+Swiper.use([Navigation, Pagination]);
+
+const pagination = {
+  el: '.swiper-pagination'
+};
+const navigation = {
+  nextEl: '.swiper-next',
+  prevEl: '.swiper-prev'
+};
+new Swiper('.banner-slider', {
+  loop: true,
+  pagination
+});
+
+new Swiper('.partners-slider', {
+  loop: true,
+  navigation,
+
+  spaceBetween: 24,
+  slidesPerView: 'auto',
+});
+
+new Swiper('.solutions-slider', {
+  loop: true,
+  navigation
+});
+
+new Swiper('.cooperation-slider', {
+  loop: true,
+  navigation
+});
+
+new Swiper('.cases-slider', {
+  loop: true,
+  navigation
+});
 
 console.log('hello');
